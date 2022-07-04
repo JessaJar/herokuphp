23b23f618c6d644abaea41a04628492b49f1fd7e
CHIANG JAMES CAMPOS <20014241@myrp.edu.sg>
2022-07-04 14:25:20 +0800

    commit

--- web/index.php
+++ web/index.php
@@ -1,10 +1,14 @@
 <!doctype html>
-<html lang="en">>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Activity Test Web</title>
+<title>Activity Test Web</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="Put your description here.">
+
 </head>
 <body>
-  Hello Worlds!
+Hello Worlds!
+
 </body>
 </html>
